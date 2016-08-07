ac2e83b28dcc63e577896ed40a73428a59e380d0
FrankBeatrice <Frankie@Franks-MacBook-Pro.local>
2016-08-07 03:02:10 -0400

    first commit

--- middleware/twilioNotifications.js
+++ middleware/twilioNotifications.js
@@ -5,6 +5,11 @@ var admins = require('../config/administrators.json');
 function formatMessage(errorToReport) {
   return 'Hello This a Test of the data breach system' +
       + errorToReport +' for more details.';
+
+      //if (err) run nspcheck --output checkstyle
+     // && run nsp check --output summary
+     // T.pot ('statuses/update'), { status: ""
+// function9err,data, response { cconsole.log (data) } }}
 };
 
 exports.notifyOnError = function(appError, request, response, next) {
